4b7b140a52e5c4dcbab4d98b3297e9e156036e1e
ag7621 <aaron.c.guerra@gmail.com>
2025-03-18 19:59:45 -0700

    added basic form submit function, select

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,34 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [equipment, setEquipment] = useState('');
+  const [position, setPosition] = useState('1');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(equipment);
+    console.log(position);
+    setEquipment('');
+  }
 
   return (
     <>
       <h1>47</h1>
-      <form>
-        <input type="text" />
+      <p>{equipment}</p>
+      <form onSubmit={handleSubmit}>
+        <select value={position} onChange={(e) => setPosition(e.target.value)}>
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Equipment number"
+          value={equipment}
+          onChange={(e) => setEquipment(e.target.value)}
+        />
         <button>Submit</button>
       </form>
     </>
