a1977bbfe6524802b35bea3d81ee85602dae3f57
ag7621 <aaron.c.guerra@gmail.com>
2025-03-19 21:55:36 -0700

    added list, entry variable to submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
   const [equipment, setEquipment] = useState('');
   const [position, setPosition] = useState('1');
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    const newEntry = {
+      id: equipment,
+      equipment: equipment,
+      position: position,
+    };
+
+    setData((prevData) => [...prevData, newEntry]);
     console.log(equipment);
     console.log(position);
     setEquipment('');
   }
 
+  useEffect(() => console.log('data checK: ', data), [data]);
+
   return (
     <>
       <h1>47</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.equipment}>
+            <p>{item.equipment}</p>
+            <p>{item.position}</p>
+          </li>
+        ))}
+      </ul>
       <p>{equipment}</p>
       <form onSubmit={handleSubmit}>
         <select value={position} onChange={(e) => setPosition(e.target.value)}>
